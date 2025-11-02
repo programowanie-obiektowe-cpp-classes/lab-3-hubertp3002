ef4e6e8b376838a14fb195c9842bdbc2386058b9
hubertp3002 <huberto1571410@gmail.com>
2025-11-02 21:32:18 +0100

    hej zegluj

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -2,8 +2,44 @@
 
 #include "Stocznia.hpp"
 
+#include <print>
+
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
+    if(towar == 0)
+        {
+        return 0;
+        }
+    else {
+        int licznik_towaru = 0;
+        int licznik_zagli = 0;
+
+        Stocznia stocznia{};
+       
+
+        
+            
+        while (licznik_towaru < towar) {
+            Statek* s1 = stocznia();
+
+            licznik_towaru += s1->transportuj();
+
+            if (dynamic_cast< Zaglowiec* >(s1)) {
+
+                licznik_zagli += 1;
+
+            }
+
+
+            delete s1;
+
+        }
+
+
+
+        return licznik_zagli;
+    }
+
+
     return 0;
 }
